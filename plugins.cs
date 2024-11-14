37f875bd45166f336ca6ea8a3253094386a884ef
Adson Felipe <59034291+AdsonF7@users.noreply.github.com>
2024-11-14 20:09:44 -0300

    Create plugins.cs

---
+++ plugins.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.GraphicsInterface;
+using Autodesk.AutoCAD.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Polyline = Autodesk.AutoCAD.DatabaseServices.Polyline;
+using Autodesk.Civil.DatabaseServices;
+using Autodesk.AutoCAD.Geometry;
+using DBObject = Autodesk.AutoCAD.DatabaseServices.DBObject;
+
+
+namespace Plugins {
+  public class Plugins {
+  }
+}
