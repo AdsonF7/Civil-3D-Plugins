44493df8b82045d0377d7b91922aae0a795c7eee
Adson Felipe <59034291+AdsonF7@users.noreply.github.com>
2024-11-17 22:22:56 -0300

    Update setxdata.cs

--- setxdata.cs
+++ setxdata.cs
@@ -2,7 +2,26 @@ namespace Plugins
 {
   public void SetXData(DBObject dbobject, List<Tuple<string, dynamic, int>> data, Transaction tr)
   {
+    Database db = dbobject.Database;
+    RegAppTable regaapptable = (RegAppTable)tr.GetObject(db.RegAppTableId, OpenMode.ForRead);
+    ResultBuffer resultbuffer = new ResultBuffer(new TypedValue((int)DxfCode.ExtendedDataRegAppName, regAppName));
 
+    if (!regaapptable.Has(regAppName))
+    {
+      regaapptable.UpgradeOpen();
+      RegAppTableRecord regapptablerecord = new RegAppTableRecord();
+      regapptablerecord.Name = regAppName;
+      regaapptable.Add(regapptablerecord);
+      tr.AddNewlyCreatedDBObject(regapptablerecord, true);
+    }
+
+    foreach (Tuple<string, dynamic, int> item in data)
+    {
+      resultbuffer.Add(new TypedValue((int)DxfCode.ExtendedDataAsciiString, item.Item1));
+      resultbuffer.Add(new TypedValue(item.Item3, item.Item2));
+    }
+
+    dbobject.XData = resultbuffer;
+    resultbuffer.Dispose();
   }
-
 }
